4821b5fc13eaa3353cb719c7582546c8d4ed7037
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2019-01-20 00:53:01 +0100

    Compile and run haskell code

--- src/Haskell.hs
+++ src/Haskell.hs
@@ -2,5 +2,33 @@ module Haskell
   ( run
   ) where
 
+import System.Process (readProcessWithExitCode)
+import System.Exit
+
+compileCmd :: FilePath -> (FilePath, [String])
+compileCmd program = ("/home/pez/.local/bin/stack", ["ghc", program])
+
+type ErrorMessage = String
+
+compileResult :: (ExitCode, String, String) -> Either ErrorMessage FilePath
+compileResult (ExitSuccess, _, _) = Right "ok"
+compileResult (ExitFailure _, _, errorMessage) = Left errorMessage
+
+compile :: FilePath -> IO (Either ErrorMessage FilePath)
+compile program = do
+  let (compiler, args) = compileCmd program
+  compileResult <$> readProcessWithExitCode compiler args ""
+
+runResult :: (ExitCode, String, String) -> String
+runResult (ExitSuccess, stdout, _) = stdout
+runResult (ExitFailure _, _, errorMessage) = errorMessage
+
+runWithInput :: FilePath -> String -> IO String
+runWithInput executable input = runResult <$> readProcessWithExitCode executable [] input
+
 run :: String -> String -> IO String
-run program _ = return "4"
+run program input = do
+  err <- compile program
+  case err of Right executable -> runWithInput executable input
+              Left errorMessage -> return errorMessage
+
