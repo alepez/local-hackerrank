5c52fc8b26b5bfba25314efe02ac5f8c41e79d7c
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2019-01-19 23:26:14 +0100

    Add cli options parser

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,35 @@
 module Lib
-    ( localHackerRank
-    ) where
+  ( localHackerRank
+  ) where
+
+import Control.Applicative
+import Options
+
+data MainOptions = MainOptions
+  { optLang :: String
+  , optProgram :: String
+  , optInput :: String
+  , optOutput :: String
+  }
+
+instance Options MainOptions where
+    defineOptions = pure MainOptions
+        <*> simpleOption "lang" "hs"
+            "Program language"
+        <*> simpleOption "program" "code.hs"
+            "Program file"
+        <*> simpleOption "input" "input.txt"
+            "Input file"
+        <*> simpleOption "output" "output.txt"
+            "Output file"
+
+foo :: MainOptions -> IO ()
+foo opts = do
+  let lang = optLang opts
+  let program = optProgram opts
+  let inputFile = optInput opts
+  let outputFile = optOutput opts
+  print [lang, program, inputFile, outputFile]
 
 localHackerRank :: IO ()
-localHackerRank = putStrLn "localHackerRank"
+localHackerRank = runCommand $ \opts _ -> foo opts
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,10 @@
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import Lib as LocalHackeRank
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
+  describe "LocalHackeRank" $ do
+    it "bla bla bla" $ do
       head [23 ..] `shouldBe` (23 :: Int)
-
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
-
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
