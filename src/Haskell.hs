4c66707f20f507551a869b03f5ea8ed3ae96d39b
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2019-01-20 01:16:49 +0100

    Fix stack path

--- src/Haskell.hs
+++ src/Haskell.hs
@@ -2,12 +2,12 @@ module Haskell
   ( run
   ) where
 
-import System.Process (readProcessWithExitCode)
 import System.Exit
 import System.FilePath.Posix (dropExtension)
+import System.Process (readProcessWithExitCode)
 
 compileCmd :: FilePath -> (FilePath, [String])
-compileCmd program = ("/home/pez/.local/bin/stack", ["ghc", program])
+compileCmd program = ("stack", ["ghc", program])
 
 type ErrorMessage = String
 
@@ -26,11 +26,12 @@ runResult (ExitSuccess, stdout, _) = stdout
 runResult (ExitFailure _, _, errorMessage) = errorMessage
 
 runWithInput :: FilePath -> String -> IO String
-runWithInput executable input = do
+runWithInput executable input =
   runResult <$> readProcessWithExitCode executable [] input
 
 run :: String -> String -> IO String
 run program input = do
   compiled <- compile program
-  case compiled of Right executable -> runWithInput executable input
-                   Left errorMessage -> return errorMessage
+  case compiled of
+    Right executable -> runWithInput executable input
+    Left errorMessage -> return errorMessage
